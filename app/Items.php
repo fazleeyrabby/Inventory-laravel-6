84397c64976dbae855c362a8d1c06071031b7094
fazleeyrabby <fazleyrabbicse@gmail.com>
2020-03-13 01:42:03 -0500

    insert

---
+++ app/Items.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Items extends Model
+{
+   	protected $table="items";
+    protected $primaryKey="id";
+    protected $fillable=[
+        'brand_id',
+        'model_id',
+        'name',
+        'entry_date',
+    ];
+}
